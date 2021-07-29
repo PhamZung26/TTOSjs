f750a713ca9c9331c14ddf5d626dbc56f7837896
PhamZung26 <37917861+PhamZung26@users.noreply.github.com>
2021-07-29 22:44:10 +0700

    Create TTOS.js
    new file

---
+++ TTOS.js
@@ -0,0 +1,67 @@
+// ==UserScript==
+// @name         TTOS
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       Phạm Dũng
+// @include      https://ttos.tcis.co/*
+// @icon         https://www.google.com/s2/favicons?domain=tampermonkey.net
+// @grant        none
+// ==/UserScript==
+
+
+ 'use strict';
+const interval = setInterval(function() {
+
+    var listContAtBay, listMethodAtList, listContAtList, containerNo;
+
+
+    listContAtBay = document.getElementsByClassName("cell-yard cell-container");
+
+
+    for (let i = 0; i < listContAtBay.length; i += 1) {
+        containerNo = listContAtBay[i].children[1].innerHTML;
+        listMethodAtList = document.getElementsByClassName("chat-list-item-photo");
+        listContAtList = document.getElementsByClassName("chat-list-item-header");
+        for(let j=0; j<listContAtList.length; j += 1){
+            if (containerNo == listContAtList[j].children[0].children[0].children[0].innerHTML) {
+                //console.log(containerNo);
+                for(let k=0; k<5; k +=1){
+                    if(listMethodAtList[j].children[k].className == ""){
+                        //Neu Class của method at list là "" thì đó là index của phương án đó
+                        // k = 0 là lệnh qua cổng
+                        // k = 1 là lệnh yard đảo chuyển
+                        // k = 2 là lệnh xuất tàu
+                        // k = 3 là lệnh nhập tầu
+                        // k = 4 là lệnh depot
+                        //console.log(containerNo);
+                        //console.log(k);
+	                    switch(k) {
+	                        case 0:
+	                            document.getElementsByClassName("cell-yard cell-container")[i].style.backgroundColor = "yellow";
+	                            break;
+	                        case 1:
+	                            document.getElementsByClassName("cell-yard cell-container")[i].style.backgroundColor = "green";
+	                            break;
+	                        case 2:
+	                            document.getElementsByClassName("cell-yard cell-container")[i].style.backgroundColor = "#ff00ea";
+	                            break;
+	                        case 3:
+	                            document.getElementsByClassName("cell-yard cell-container")[i].style.backgroundColor = "red";
+	                            break;
+	                        case 4:
+	                            document.getElementsByClassName("cell-yard cell-container")[i].style.backgroundColor = "lightblue";
+	                            break;
+	                        default:
+	                            document.getElementsByClassName("cell-yard cell-container")[i].style.backgroundColor = "black";
+	                    }
+                   break;
+                    }
+
+
+                }
+            }
+        }
+    }
+
+ }, 3000);
