8a8ce4b9cd25655cffbef94675830812d31aff21
PhamZung26 <37917861+PhamZung26@users.noreply.github.com>
2021-07-31 20:34:54 +0700

    Create block.js
    create block site

---
+++ block.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         family
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @include       *
+// @exclude      https://ttos.tcis.co/*
+// @icon         https://www.google.com/s2/favicons?domain=tcis.co
+// @grant        none
+// ==/UserScript==
+
+
+    'use strict';
+alert("Cấm mở");
+window.location.href = "https://ttos.tcis.co";
+
+
