d33649929916fe46db14e175b9f9f336fe692cff
Phạm Văn Dũng <mr.phamdung26@gmail.com>
2021-08-01 12:38:58 +0700

    Tô màu tùy ý theo người dùng

--- TTOS.js
+++ TTOS.js
@@ -12,14 +12,13 @@
 
  'use strict';
 var containerNo_booked = [];
-var data = [];
 const interval_getlistBooked = setInterval(function() {
 (async () => {
     fetch("https://spreadsheets.google.com/feeds/list/17JfxIWPJsNIisQFXu_lJvn_Vjb4b2oG3EeJ_3dZMk3Q/1/public/values?alt=json")
   .then(res => res.json())
   .then(json => {
      /* this array will eventually be populated with the contents of the spreadsheet's rows */
-      data = [];
+      const data = [];
       containerNo_booked = [];
     const rows = json.feed.entry;
 
@@ -56,6 +55,52 @@ const interval_getlistBooked = setInterval(function() {
     console.log(containerNo_booked);
   })
 })();
+}, 15000);
+
+var container_color = [];
+var colorOfContainer = [];
+const interval_getlistCustomColor = setInterval(function () {
+    (async () => {
+        fetch("https://spreadsheets.google.com/feeds/list/1yzxWwqqOsPINsbWLCMOZYt2cGNddXnfaqgqZqk4TgkE/1/public/values?alt=json")
+            .then(res => res.json())
+            .then(json => {
+                /* this array will eventually be populated with the contents of the spreadsheet's rows */
+                const data = [];
+                container_color = [];
+                colorOfContainer = [];
+                const rows = json.feed.entry;
+
+                for (const row of rows) {
+                    const formattedRow = {};
+
+                    for (const key in row) {
+                        if (key.startsWith("gsx$")) {
+
+                            /* The actual row names from your spreadsheet
+                             * are formatted like "gsx$title".
+                             * Therefore, we need to find keys in this object
+                             * that start with "gsx$", and then strip that
+                             * out to get the actual row name
+                             */
+
+                            formattedRow[key.replace("gsx$", "")] = row[key].$t;
+
+                        }
+                    }
+
+                    data.push(formattedRow);
+                }
+
+                var colorHex = Object.getOwnPropertyNames(data[0])[0];
+                var containerUserCustumColor = Object.getOwnPropertyNames(data[0])[2];
+                for (let n = 0; n < data.length; n++) {
+                    container_color[n] = data[n][containerUserCustumColor];
+                    colorOfContainer[n] = data[n][colorHex];
+                }
+                console.log(colorOfContainer);
+                console.log(container_color);
+            })
+    })();
 }, 15000);
 
 
@@ -116,6 +161,15 @@ const interval = setInterval(function() {
                 }
             }
         }
+        if (container_color.indexOf(containerNo) != -1) {
+            // Nếu containerNo tồn tại trong list người dùng custom thì tô màu theo ý người dùng
+            if (colorOfContainer[container_color.indexOf(containerNo)] != "#ffffff") {
+                // Nếu người dùng tô màu trắng thì không tô lại, để màu mặc định của TTOS
+                document.getElementsByClassName("cell-yard cell-container")[i].style.backgroundColor = colorOfContainer[container_color.indexOf(containerNo)];
+            }
+            
+        }
+
     }
  }, 3000);
 
