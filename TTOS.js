430603cba7455209fb26ddde5cf844ea5e704dd4
PhamZung26 <37917861+PhamZung26@users.noreply.github.com>
2021-08-25 11:07:09 +0700

    Update TTOS.js
    Kéo dài thời gian lấy dữ liệu, tránh hết data

--- TTOS.js
+++ TTOS.js
@@ -11,12 +11,21 @@
 
 
 'use strict';
+document.getElementById("txt-search-cont").setAttribute("type", "number"); // Gửi lệnh này ở đây để set Type của textbox tìm kiếm là số
+document.getElementById("search-workinstruction").setAttribute("type", "number"); // Gửi lệnh này ở đây để set Type của textbox tìm kiếm là số
 var containerNo_booked = [];
 var containerNo_store = [];
 var currentLocation = [];
-const interval_getlistBooked = setInterval(async function() {
+const interval_getlistBooked = new timer();
+interval_getlistBooked.start(async function() {
     let rows = await fetch("https://sheets.googleapis.com/v4/spreadsheets/17JfxIWPJsNIisQFXu_lJvn_Vjb4b2oG3EeJ_3dZMk3Q/values/TonBai?key=AIzaSyCTp0GCp6TyvlU0VGfXbjaiLV-N6LECK2Y")
-    .then(r => r.json())
+    .then( r => r.json())
+    if (containerNo_store.length > 1){
+    	interval_getlistBooked.set_interval(900000);
+    	// nếu có dữ liệu rồi thì 15 phút mới lấy dữ liệu 1 lần	
+    }else{
+    	interval_getlistBooked.set_interval(1000);	
+    }
     containerNo_booked = [];
     containerNo_store = [];
     currentLocation = [];
@@ -28,15 +37,31 @@ const interval_getlistBooked = setInterval(async function() {
             containerNo_booked.push(row[0]);
         }
       }
+      console.log(new Date());
+      console.log(containerNo_store);
+
 }
-, 30000);
+, 1000, true);
+
+
+
 
 var container_color = [];
 var colorOfContainer = [];
-const interval_getlistCustomColor =  setInterval(async function () {
+
+const interval_getlistCustomColor = new timer();
+interval_getlistCustomColor.start(
+	async function () {
 
     let rows = await fetch("https://sheets.googleapis.com/v4/spreadsheets/1yzxWwqqOsPINsbWLCMOZYt2cGNddXnfaqgqZqk4TgkE/values/XuatTau?key=AIzaSyCTp0GCp6TyvlU0VGfXbjaiLV-N6LECK2Y")
     .then(r => r.json())
+
+    if (container_color.length > 1){
+    	interval_getlistCustomColor.set_interval(15000);
+    	// nếu có dữ liệu rồi thì 15 s mới lấy dữ liệu 1 lần	
+    }else{
+    	interval_getlistCustomColor.set_interval(1000);	
+    }
     container_color = [];
     colorOfContainer = [];
       for(const row of rows.values){
@@ -53,7 +78,9 @@ const interval_getlistCustomColor =  setInterval(async function () {
               colorOfContainer.push(row[0]);
           }
         }
-  }, 30000);
+      console.log(new Date());
+      console.log(container_color);
+  }, 1000, true);
 
   function insertAfter(referenceNode, newNode) {
     referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
@@ -63,6 +90,38 @@ const interval_getlistCustomColor =  setInterval(async function () {
     return new Promise(resolve => setTimeout(resolve, ms));
   }
 
+
+function timer()
+{
+var timer = {
+    running: false,
+    iv: 5000,
+    timeout: false,
+    cb : function(){},
+    start : function(cb,iv,sd){
+        var elm = this;
+        clearInterval(this.timeout);
+        this.running = true;
+        if(cb) this.cb = cb;
+        if(iv) this.iv = iv;
+        if(sd) elm.execute(elm);
+        this.timeout = setTimeout(function(){elm.execute(elm)}, this.iv);
+    },
+    execute : function(e){
+        if(!e.running) return false;
+        e.cb();
+        e.start();
+    },
+    stop : function(){
+        this.running = false;
+    },
+    set_interval : function(iv){
+        clearInterval(this.timeout);
+        this.start(false, iv);
+    }
+};
+return timer;
+}
   var searchCont = document.createElement("BUTTON");
   searchCont.setAttribute("id", "searchCont");
   searchCont.innerHTML = "Tìm cont";
@@ -107,7 +166,7 @@ const interval_getlistCustomColor =  setInterval(async function () {
 const interval = setInterval(function() {
 
     var listContAtBay, listMethodAtList, listContAtList, containerNo;
-    document.getElementById("txt-search-cont").setAttribute("type", "number"); // Gửi lệnh này ở đây để set Type của textbox tìm kiếm là số
+
 
 
     listContAtBay = document.getElementsByClassName("cell-yard cell-container");
